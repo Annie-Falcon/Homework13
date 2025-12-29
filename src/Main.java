08ef0f14a4dfb0f202dee7211d40601fa922a24d
Annie-Falcon <anna81283@mail.ru>
2025-12-29 22:22:14 +0300

    Done homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        Product product1 = new Product(883456,"книга в твердой обложке", 730, "книга");
+        Product product2 = new Product(794120,"обложка для паспорта", 480, "аксессуары");
+        Product product3 = new Product(883456,"книга, обложка твердая ", 735, "книга");
 
+        System.out.println("Товары:");
+        System.out.println(product1);
+        System.out.println(product2);
+        System.out.println(product3);
+
+        System.out.println("Соответствие первого и второго товара " + product1.equals(product2));
+        System.out.println("Соответствие первого и третьего товара " + product1.equals(product3));
+
+
+        Product product4 = new Product(695130,"ручка", 150, "канцелярия");
+        Product product5 = new Product(873354,"карандаш ", 173, "канцелярия");
+
+        Order order1 = new Order("Покупатель 1", new Product[] {product1 , product2 , product4});
+        Order order2 = new Order("Покупатель 2", new Product[] {product3 , product2 , product5});
+        Order order3 = new Order("Покупатель 1", new Product[] {product3 , product2 , product4});
+
+        System.out.println("Заказы:");
+        System.out.println(order1);
+        System.out.println(order2);
+        System.out.println(order3);
+
+        System.out.println("Сравнение первого и второго заказа: " + order1.equals(order2));
+        System.out.println("Сравнение первого и третьего заказа: " + order1.equals(order3));
     }
 }
---
+++ src/Order.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Order {
+    public String customer;
+    public Product[] basket;
+
+    public Order(String customer, Product[] basket) {
+        this.customer = customer;
+        this.basket = basket;
+    }
+
+    @Override
+    public String toString() {
+        return "Корзина покупателя " + this.customer + " " + Arrays.toString(this.basket);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        int i = 0;
+        if (Objects.equals(customer, order.customer)) {
+            for (final Product current : basket) {
+                if (current == null) {
+                    return false;
+                } else if (current.equals(order.basket[i])) {
+                    i ++;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        } else return false;
+    }
+}
